ce01d308952171f4573b026106e1c80b376656f9
Kyle  Beckstead <kyle21beckstead@gmail.com>
2018-12-04 17:26:33 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,6 @@
+def introduction(name)
+  puts "Hi, my name is Josh."
+end
+def introduction_with_language(name,language)
+  puts "Hi, my name is Dan and I am learning to program in Ember.js.\n"
+end
