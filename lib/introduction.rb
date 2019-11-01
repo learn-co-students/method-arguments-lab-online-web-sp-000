00927c846edf8f84f74c03950e051668e99da64e
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-01 20:47:07 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,14 @@
+def introduction(name)
+  
+  puts "Hi, my name is #{name}.\n"
+  
+end
+
+#Hi, my name is Dan and I am learning to program in Ember.js.\n"
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
+
+introduction('Josh')
+introduction_with_language('Dan', 'Ember.js')
