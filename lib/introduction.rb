6134c39a57774055a50261643709dee12e843743
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-28 19:52:04 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(me, language)
+  puts "Hi, my name is #{me} and I am learning to program in #{language}."
+end
