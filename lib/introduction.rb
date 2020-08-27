f889bc0b251e2ccd1817531bdeccc3251247388d
Christerle Nounoute <christerlesn@gmail.com>
2020-08-27 00:22:24 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+puts introduction("Sam")
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+puts introduction_with_language("Josh", "Javascript")
