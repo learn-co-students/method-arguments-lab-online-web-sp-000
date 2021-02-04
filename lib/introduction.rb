60a1c467d597463b7b09fffc8b35c7e1dcb212f2
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-04 21:09:23 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name = Dan, language = Ember)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+
