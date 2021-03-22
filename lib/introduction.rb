6d12aebae41ed9a5bc66fe7929c636746459747a
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-03-22 21:52:37 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,20 @@
+
+def name(name)
+  puts ("Josh")
+end
+
+def introduction (name)
+  puts "Hi, my name is #{name}."
+end
+
+def name(name)
+  puts ("Dan")
+end
+
+def language(language)
+  puts ("Ember.js")
+end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
